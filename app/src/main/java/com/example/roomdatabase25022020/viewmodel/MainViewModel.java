dc2527292230d3ed679a8d887641468dbd743778
phatdroid94 <phatdroid94@gmail.com>
2020-05-23 20:51:29 +0700

    finish

--- app/src/main/java/com/example/roomdatabase25022020/viewmodel/MainViewModel.java
+++ app/src/main/java/com/example/roomdatabase25022020/viewmodel/MainViewModel.java
@@ -1,10 +1,14 @@
 package com.example.roomdatabase25022020.viewmodel;
 
 import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.OnLifecycleEvent;
 import androidx.lifecycle.ViewModel;
 
 import com.example.roomdatabase25022020.model.entity.Sinhvien;
@@ -14,11 +18,13 @@ import java.util.List;
 
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainViewModel extends ViewModel implements LifecycleObserver {
     private MutableLiveData<List<Sinhvien>> sinhvienList;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     public MainViewModel() {
         this.sinhvienList = new MutableLiveData<>();
@@ -37,7 +43,7 @@ public class MainViewModel extends ViewModel implements LifecycleObserver {
                 .subscribe(new Observer<List<Sinhvien>>() {
                     @Override
                     public void onSubscribe(Disposable d) {
-
+                        compositeDisposable.add(d);
                     }
 
                     @Override
@@ -55,5 +61,11 @@ public class MainViewModel extends ViewModel implements LifecycleObserver {
 
                     }
                 });
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    public void clearSubscription(){
+        compositeDisposable.dispose();
+        compositeDisposable.clear();
     }
 }
